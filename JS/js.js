2ed4a4c7a98333346a7e58027d0e70dfbe5a2fcf
NikoDMSofia <damyan_svetiev@abv.bg>
2025-08-14 19:20:21 +0300

    Add files via upload

---
+++ JS/js.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'flex'
+        }
+
+        function showSidebar_closed() {
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'none'
+        }
+  
